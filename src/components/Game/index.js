98051e7d14276750e4f1b53aab6dcca031097655
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-20 23:06:36 -0300

    Add swipe controls for phone users

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -15,6 +15,37 @@ export default function Game() {
 			else if (e.key.toLowerCase() === 'a') setLeft(0);
 			else if (e.key.toLowerCase() === 's') setLeft(33.33);
 		};
+
+		let xDown = null;
+		let yDown = null;
+
+		document.addEventListener('touchstart', handleTouchStart, false);
+		document.addEventListener('touchmove', handleTouchMove, false);
+
+		const getTouches = (evt) => evt.touches || evt.originalEvent.touches;
+		function handleTouchStart(evt) {
+			const firstTouch = getTouches(evt)[0];
+			xDown = firstTouch.clientX;
+			yDown = firstTouch.clientY;
+		}
+
+		function handleTouchMove(evt) {
+			if (!xDown || !yDown) return;
+
+			let xUp = evt.touches[0].clientX;
+			let yUp = evt.touches[0].clientY;
+
+			let xDiff = xDown - xUp;
+			let yDiff = yDown - yUp;
+
+			if (Math.abs(xDiff) > Math.abs(yDiff)) {
+				if (xDiff > 0) setLeft((prev) => (prev === 0 ? prev : prev - 33.33));
+				else setLeft((prev) => (prev === 66.66 ? prev : prev + 33.33));
+			}
+
+			xDown = null;
+			yDown = null;
+		}
 	}, []);
 
 	return (
