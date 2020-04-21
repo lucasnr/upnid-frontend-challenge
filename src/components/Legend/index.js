12865b67c0b58450c0dc1d77f2b28c0a91530025
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-21 13:47:24 -0300

    Add better controls

--- src/components/Legend/index.js
+++ src/components/Legend/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { isMobile } from 'react-device-detect';
 
 import { Container, Name, Controls } from './styles';
 
 export default function Legend({ name }) {
+	const isPhone = useMemo(() => {
+		return navigator.userAgent;
+	}, []);
+	console.log(isPhone);
+
 	return (
 		<Container>
 			<Name>{name || 'Jogador sem nome'}</Name>
 			<Controls>
-				<h5>A</h5>
-				<h6>Ir para o lado esquerdo</h6>
-				<h5>S</h5>
-				<h6>Ir para o centro</h6>
-				<h5>D</h5>
-				<h6>Ir para o lado direito</h6>
-				<h5>Seta esquerda</h5>
-				<h6>Ir para a esquerda</h6>
-				<h5>Seta direita</h5>
-				<h6>Ir para a direita</h6>
-				<h5>ESC</h5>
-				<h6>Pausar jogo</h6>
+				{isMobile ? (
+					<>
+						<h5>Deslizar para esquerda</h5>
+						<h6>Ir para a esquerda</h6>
+						<h5>Deslizar para direita</h5>
+						<h6>Ir para a direita</h6>
+					</>
+				) : (
+					<>
+						<h5>A</h5>
+						<h6>Ir para o lado esquerdo</h6>
+						<h5>S</h5>
+						<h6>Ir para o centro</h6>
+						<h5>D</h5>
+						<h6>Ir para o lado direito</h6>
+						<h5>Seta esquerda</h5>
+						<h6>Ir para a esquerda</h6>
+						<h5>Seta direita</h5>
+						<h6>Ir para a direita</h6>
+						<h5>ESC</h5>
+						<h6>Pausar jogo</h6>
+					</>
+				)}
 			</Controls>
 		</Container>
 	);
